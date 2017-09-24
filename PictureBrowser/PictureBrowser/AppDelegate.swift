90195cf14f6c674fc5a5ab57dc8d8ce53c23d2f3
liuqing <liuqing@qianjiangcloud.com>
2017-09-24 11:55:07 +0800

    项目初步搭建

---
+++ PictureBrowser/PictureBrowser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PictureBrowser
+//
+//  Created by liuqing on 2017/9/24.
+//  Copyright © 2017年 liuqing. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.white
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ PictureBrowser/PictureBrowser/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PictureBrowser
+//
+//  Created by liuqing on 2017/9/24.
+//  Copyright © 2017年 liuqing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
